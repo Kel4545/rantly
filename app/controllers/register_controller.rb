937257679132454463df9c9605cbb778b4c7cb66
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-08-19 14:51:30 -0600

    Added Join Link

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,6 @@
+class RegisterController < ApplicationController
+  def register
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "rants#index"
+  get "signin" => "sessinons#new", as: :signin
+  post "signin" => "sessions#create"
+  get "signout" => "sessions#destroy", as: :signout
+  get "register" => "register#register", as: :register
   end
