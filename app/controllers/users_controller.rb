f2b168023d3f237b483f740c0d93aa207c7c2fbc
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-04 14:50:17 -0600

    Updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
     @user = User.all
 
     def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      @user.update_attributes(
+        username: params[:user][:username],
+        firstname: params[:user][:firstname],
+        lastname: params[:user][:lastname],
+        bio: params[:user][:bio],
+        password: params[:user][:password])
+      redirect_to "/user/#{@user.id}"
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
 
     end
   end
