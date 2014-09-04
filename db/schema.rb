32735bb0df6769412bac961c576907b2dcd6963c
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-04 13:36:39 -0600

    Working on login page

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20140819211049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "rants", force: true do |t|
+  create_table "dashboard", force: true do |t|
     t.string "rant"
     t.string "user_id"
   end
