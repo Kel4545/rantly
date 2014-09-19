bbad30291a1a7ed537c58bd6d3de73745ed239be
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-19 11:41:23 -0600

    Working on follows controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,5 +2,25 @@ class FollowsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
+    @rant = Rant.new
+    @rants = Rant.all
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @follow = Follow.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @follow = Follow.new(accepted_params)
+    if @follow.save
+      redirect to dashboard_path(@user.id)
+    else
+      redirect_to dashboard_path(@user.id)
+    end
   end
 end
+
+
+
