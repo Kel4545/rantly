4b9f3d9eb00e897b4ce5bfe37114448adc447f7a
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-03 15:41:01 -0600

    Working on evil CSS of dashboard show page

--- spec/Features/dashboard_spec.rb
+++ spec/Features/dashboard_spec.rb
@@ -5,7 +5,7 @@ feature 'Home Page' do
 
   scenario 'Unregistered user can view homepage' do
     visit root_path
-    click_link("Login")
+    click_on("Rant.ly")
     expect(page).to have_link("Join")
     expect(page).to have_content("Quote")
   end
