1027de6f521834aa641d38053a23b64e61c69b42
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-16 09:53:07 -0600

    Fixing CSS on dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => "http://critterbabies.com/wp-content/gallery/kittens/happy-kitten-kittens-5890512-1600-1200.jpg"
+  has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>", :convert_options => {:thumb => "-quality 75 -strip" }}, :default_url => "http://critterbabies.com/wp-content/gallery/kittens/happy-kitten-kittens-5890512-1600-1200.jpg"
   validates_attachment :avatar, presence: true, :content_type => {:content_type => ["image/jpeg", "image/gif", "image/png"]}
 
   has_many :rants
