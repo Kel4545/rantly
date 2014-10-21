c13d6d0f1d6579bb027cea719296728374700d76
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-20 18:04:28 -0600

    Fixed search path

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,6 @@
 class SearchController < ApplicationController
 
-  def new
-    @user = User.find(session[:user_id])
-    @rant = Rant.new
-  end
+
 
   def index
       @users = User.where("first_name LIKE ? OR last_name LIKE ? OR username LIKE ?", "%#{params[:search]}%","%#{params[:search]}%", "%#{params[:search]}%")
