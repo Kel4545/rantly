4bcf4f9b2c1be051707723c2720aa85e5543c9a8
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-20 18:58:34 -0600

    Working on search form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,9 @@ class SearchController < ApplicationController
 
   def index
     params[:search] ||= "alskdfjlakjsdfihoqw}}KQEPifj"
+    @content = Rant.where('content LIKE ?', "%"+params[:search]+"%")
     @rants = Rant.joins(:user).where('username LIKE ? OR firstname LIKE ? OR lastname LIKE ?', params[:search]+"%", params[:search]+"%", params[:search]+"%")
-    @title = Rant.where('title LIKE ?', params[:search]+"%")
-    @contents = Rant.where('content LIKE ?', "%"+params[:search]+"%")
+    @title = Rant.where('title LIKE ?', params[:search]+ "%")
     @users = User.where("firstname LIKE ? OR lastname LIKE ? OR username LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
     @favorite = Favorite.where((:user_id == current_user.id) && (:rant_id == params[:rant_id])).first
     @user = User.find(session[:user_id])
