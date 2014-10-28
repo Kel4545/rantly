75e45430d51ac013158c87e5577b78e202f12bab
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-28 13:52:56 -0600

    WOrking on email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,8 +51,8 @@ Rails.application.configure do
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :domain               => "gmail.com",
-      :user_name            => "is this showing?",
-      :password             => hmmmm,
+      :user_name            => ENV['GMAIL_USERNAME_DEV'],
+      :password             => ENV['GMAIL_PASSWORD_DEV'],
       :authentication       => "plain",
       :enable_starttls_auto => true
     }
