b5cdf8c8e5e5450781e71901bf9cd6667c44ad24
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-27 19:12:19 -0600

    Working on admin functions

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,24 @@
+class AdminsController < ApplicationController
+before_action :check_if_admin
+
+def show
+  @rants = Rant.order('created_at DESC')
+end
+
+def rants
+  @rants = Rant.order('created_at DESC')
+end
+
+def users
+  @users = User.all
+end
+
+private
+
+def check_if_admin
+  unless current_user.admin
+    redirect_to "/"
+  end
+end
+
+end
