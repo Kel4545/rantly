11d939ac66368e2f14c132f73ceeeb2a2e19073c
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-31 14:26:09 -0600

    Working on email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :check_signed_in, except: :destroy
+  # before_action :check_signed_in, except: :destroy
 
   def new
     @user = User.new
@@ -22,12 +22,12 @@ class SessionsController < ApplicationController
     redirect_to signin_path
   end
 
-  private
-
-  def check_signed_in
-    if signed_in?
-      flash.now.alert = "Already signed in"
-      redirect_to root_path
-    end
-  end
+  # private
+  #
+  # def check_signed_in
+  #   if signed_in?
+  #     flash.now.alert = "Already signed in"
+  #     redirect_to root_path
+  #   end
+  # end
 end
