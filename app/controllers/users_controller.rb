e84c55db1abb7e290d2bac9eddace1048148a9e2
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-11-01 09:18:35 -0600

    Database error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,44 +17,44 @@ class UsersController < ApplicationController
   def create
     @user = User.new(accepted_params)
 
-      if @user.save!
-        set_cookie
-        Keen.publish(:sign_ups, {username: @user.username, date: @user.created_at}) if Rails.env.production?
-        UserMailer.welcome_email(@user).deliver
-        @user.send_confirmation
-        flash[:notice] = "Thank you for registering!"
-        redirect_to root_path
-      else
-        flash[:notice] = "Please try and register again"
-        render :new, :layout => "root"
-      end
-    end
-  end
-
-
-  def edit
-    @user = current_user
-    render :layout => "edit"
-  end
-
-  def update
-    @user = current_user
-    @user.update_attributes(accepted_params)
-    if @user.save
-      redirect_to dashboard_path(@user)
+    if @user.save!
+      set_cookie
+      Keen.publish(:sign_ups, {username: @user.username, date: @user.created_at}) if Rails.env.production?
+      UserMailer.welcome_email(@user).deliver
+      @user.send_confirmation
+      flash[:notice] = "Thank you for registering!"
+      redirect_to signin_path
     else
-      flash[:notice] = "Information not saved"
+      flash[:notice] = "Please try and register again"
+      render :new, :layout => "root"
     end
   end
+end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+
+def edit
+  @user = current_user
+  render :layout => "edit"
+end
+
+def update
+  @user = current_user
+  @user.update_attributes(accepted_params)
+  if @user.save
+    redirect_to dashboard_path(@user)
+  else
+    flash[:notice] = "Information not saved"
   end
+end
+
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+end
 
 def account_confirmation
   @user = User.find_by_password_reset_token(params[:token])
-  if(@user)
+  if (@user)
     @user.update_column(:confirmed, true)
     redirect_to login_url, :notice => "Account confirmed"
   else
