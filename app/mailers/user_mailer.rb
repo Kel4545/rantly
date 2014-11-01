8da92fde96985d6c4b5336a7788b8924ec9c3e81
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-11-01 09:56:59 -0600

    Trying to fix email bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ActionMailer::Base
-
+  default :from => "ihaterantly@gmail.com"
 
   def welcome_email(user)
     @user = user
     @url = 'http://nameless-fjord-8757.herokuapp.com/'
-    mail(to: user.email, subject: 'Welcome to Rant.ly')
+    mail(to: @user.email, from: "ihaterantly@gmail.com", subject: 'Welcome to Rant.ly')
   end
 
   def send_confirmation_mail(user)
     @user = user
-    mail(to: @user.email, subject: 'User Registration confirmation')
+    mail(to: @user.email, from: "ihaterantly@gmail.com", subject: 'User Registration confirmation')
   end
 end
 
