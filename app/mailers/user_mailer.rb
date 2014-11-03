8b2428b443048005a56cc371ec97ac26727205fa
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-11-02 17:20:34 -0700

    Working on sending emails in heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,14 +4,14 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url  = Rails.env.production? ? 'http://nameless-fjord-8757' : 'http://localhost:3000'
+    @url  = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/' : 'http://localhost:3000'
     mail(to: @user.email, subject: 'Welcome to Rant.ly')
   end
 
   def send_confirmation_mail(user)
     @token = 'something'
     @user = user
-    @url = Rails.env.production? ? 'http://nameless-fjord-8757'  : 'http://localhost:3000'
+    @url = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/'  : 'http://localhost:3000'
     mail(to: @user.email, subject: 'User Registration confirmation')
   end
 end
