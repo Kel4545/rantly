6ade79f6b50ec2353185a3fe337c23ba5331826b
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-11-03 16:42:40 -0700

    Added ranter method in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,22 +4,24 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url  = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/' : 'http://localhost:3000'
+    @url = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/' : 'http://localhost:3000'
     mail(to: @user.email, subject: 'Welcome to Rant.ly')
   end
 
   def send_confirmation_mail(user)
     @token = user.remember_token
     @user = user
-    @url = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/'  : 'http://localhost:3000'
+    @url = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/' : 'http://localhost:3000'
     mail(to: @user.email, subject: 'User Registration confirmation')
   end
 
-  def new_rant(user, rant)
-    @user = user
-    @rant = rant
-    @url = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/'  : 'http://localhost:3000'
-    mail(to: @user.email, subject: 'New rant')
+  def new_rant(ranter)
+      Following.where(followee_id: ranter.id).each do |following|
+      @user = User.find(following.follower_id)
+      @ranter = ranter
+      @url = Rails.env.production? ? 'http://nameless-fjord-8757.herokuapp.com/' : 'http://localhost:3000'
+      mail(to: @user.email, subject: "#{ranter.first_name} 'Ranted'")
+    end
   end
 end
 
