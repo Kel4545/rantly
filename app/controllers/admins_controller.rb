cba4fd56af263280aadd817a52db736c2ecf90a5
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-11-04 10:06:05 -0700

    Admin see rants ordered by date

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,11 +3,11 @@ class AdminsController < ApplicationController
   layout "edit"
 
   def show
-    @rant = Rant.new
+    @rants = Rant.order('created_at DESC')
   end
 
   def rants
-    @rants = Rant.order('created_at DESC')
+
   end
 
   def users
