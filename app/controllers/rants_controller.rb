0cb9a680a20d6b2bc8a18894c8916d699f531896
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-11-06 14:34:39 -0700

    Fixed rant controller Keen

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -21,7 +21,7 @@ class RantsController < ApplicationController
     @user = User.find(params[:user_id])
     @rant = Rant.new(accepted_params)
     if @rant.save
-      Keen.publish(:logins, {username: @user.username, date: Time.now}) if Rails.env.production?
+      Keen.publish(:rants, {username: @user.username, date: Time.now}) if Rails.env.production?
       UserMailer.new_rant(current_user).deliver
       redirect_to dashboard_path(@user.id)
     else
