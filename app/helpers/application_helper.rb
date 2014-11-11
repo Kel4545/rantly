c2f38b6989841830c4a401e7e9d50c04d31eb720
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-11-11 11:50:30 -0700

    User can search bu hashtag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,15 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :no_intra_emphasis => true)
     markdown.render(text).html_safe
   end
+
+  def hashtag_search(text)
+    hashtag = text.scan(/#\w+/)
+    hashtag.each { |tag|
+      unless tag == '#39'
+        url = tag.gsub('#', '')
+        text.gsub!(tag, "<a href='/searches?utf8=✓&search=#{url}'>#{tag}</a>")
+      end
+    }
+    text.html_safe
+  end
 end
